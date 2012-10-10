b71c953393c195eac7d8be7462fdbc0f7d119381
John Atten <xivsolutions@gmail.com>
2012-10-10 00:20:44 -0500

    Initial Commit DGV Demo Project

---
+++ dgvControl/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace dgvControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvControl1 = new dgvControl();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvControl1
+            // 
+            this.dgvControl1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvControl1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvControl1.Location = new System.Drawing.Point(12, 12);
+            this.dgvControl1.Name = "dgvControl1";
+            this.dgvControl1.Size = new System.Drawing.Size(345, 150);
+            this.dgvControl1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 170);
+            this.Controls.Add(this.dgvControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private dgvControl dgvControl1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+
+    }
+}
+
---
+++ dgvControl/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dgvControl
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //var dgv = this.dataGridView1;
+
+            //dgv.CellEnter += new DataGridViewCellEventHandler(dgv_CellEnter);
+            //_currentColumn = new DataGridViewColumn();
+            //this.OnColumnFocus(0);
+
+        }
+
+        //void dgv_CellEnter(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    this.OnColumnFocus(e.ColumnIndex);        
+        //}
+
+
+        //void OnColumnFocus(int ColumnIndex)
+        //{
+        //    var dgv = this.dataGridView1;
+
+        //    if (ColumnIndex != _currentColumn.Index)
+        //    {
+        //        Font selectedFont = new Font(dgv.Font, FontStyle.Bold);
+        //        var newColumn = dgv.Columns[ColumnIndex];
+        //        newColumn.HeaderCell.Style.Font = selectedFont;
+        //        _currentColumn.HeaderCell.Style.Font = dgv.Font;
+        //        _currentColumn = newColumn; 
+        //    }
+
+        //}
+
+
+
+    }
+}
